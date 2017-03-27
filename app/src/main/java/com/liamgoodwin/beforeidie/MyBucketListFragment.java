14d7855fc990e055a4468efcdedbaedd5fae3684
Jarrod Maeckeler <jarrodmaeckeler@gmail.com>
2017-03-26 22:05:39 -0400

    Added in the listview

--- app/src/main/java/com/liamgoodwin/beforeidie/MyBucketListFragment.java
+++ app/src/main/java/com/liamgoodwin/beforeidie/MyBucketListFragment.java
@@ -4,13 +4,25 @@ package com.liamgoodwin.beforeidie;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class MyBucketListFragment extends Fragment {
+
+    FragmentManager fm;
+    ListView list;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_bucket_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_bucket_list, container, false);
+
+        fm = getActivity().getSupportFragmentManager();
+
+        list = (ListView) view.findViewById(R.id.bucketlistListView);
+
+        return view;
     }
 }
