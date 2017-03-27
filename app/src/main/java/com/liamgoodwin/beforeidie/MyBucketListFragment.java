2f842454315600dbbf33d9cef54fdfb39218afea
Jarrod Maeckeler <jarrodmaeckeler@gmail.com>
2017-03-26 22:39:58 -0400

    Added in custom adapter and set adapter

--- app/src/main/java/com/liamgoodwin/beforeidie/MyBucketListFragment.java
+++ app/src/main/java/com/liamgoodwin/beforeidie/MyBucketListFragment.java
@@ -1,6 +1,9 @@
 package com.liamgoodwin.beforeidie;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,7 +11,10 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -26,8 +32,36 @@ public class MyBucketListFragment extends Fragment {
 
         list = (ListView) view.findViewById(R.id.bucketlistListView);
         final ArrayList<Bucketlist> bucketList = new ArrayList<Bucketlist>();
-        bucketList.add(new Bucketlist("Snorkel in The Great Barrier Reef", "The Great Barrier Reef is th largest aquatic animal habitant", 666));
+        bucketList.add(new Bucketlist("Snorkel in The Great Barrier Reef", "The Great Barrier Reef is the largest aquatic animal habitat in the world", 666));
+        final CustomAdapter adapter = new CustomAdapter(getContext(), bucketList);
+        list.setAdapter(adapter);
 
         return view;
+    }
+
+    public class CustomAdapter extends ArrayAdapter<Bucketlist> {
+
+        public CustomAdapter(Context context, ArrayList<Bucketlist> items) {
+            super(context, 0, items);
+        }
+
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            final Bucketlist item = getItem(position);
+
+            if(convertView == null){
+                convertView =
+                        LayoutInflater.from(getContext()).inflate(
+                                R.layout.card_view_item, parent, false);
+            }
+
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+            name.setText(item.getName());
+
+            return  convertView;
+        }
+
+
+
     }
 }
