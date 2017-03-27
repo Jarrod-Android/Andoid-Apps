af7e4e6a672bd981411ca178467e290859499958
Jarrod Maeckeler <jarrodmaeckeler@gmail.com>
2017-03-26 22:10:12 -0400

    Created bucklist object

---
+++ app/src/test/java/Bucketlist.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+
+/**
+ * Created by JarrodMaeckeler on 2017-03-26.
+ */
+
+public class Bucketlist {
+
+    private String name;
+    private String description;
+    private BigInteger time;
+
+    public Bucketlist(String name, String description, BigInteger time){
+        this.name = name;
+        this.description = description;
+        this.time = time;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public BigInteger getTime() { return time; }
+
+    public void setTime(BigInteger time) { this.time = time; }
+}
