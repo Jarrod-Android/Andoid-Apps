25c02f35dc592ee7f09a398c1309eaf08f77bc5d
Liam Goodwin <goodwin_liam1@stclairconnect.ca>
2017-03-27 08:49:39 -0400

    Fixing conflict between branches

---
+++ app/src/main/java/com/liamgoodwin/beforeidie/AddToMyBucketListFragment.java
@@ -0,0 +1,18 @@
+package com.liamgoodwin.beforeidie;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liamgoodwin on 2017-03-27.
+ */
+public class AddToMyBucketListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_to_my_bucket_list, container, false);
+    }
+}
