f233b3a4e87894aac7bb26d8b1d34949a74e0de0
Liam Goodwin <goodwin_liam1@stclairconnect.ca>
2017-03-30 22:22:10 -0400

    Working on getting Spinner to see bucketList names in the dropdown

--- app/src/main/java/com/liamgoodwin/beforeidie/AddPhotoFragment.java
+++ app/src/main/java/com/liamgoodwin/beforeidie/AddPhotoFragment.java
@@ -2,37 +2,34 @@ package com.liamgoodwin.beforeidie;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddPhotoFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddPhotoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import static android.app.Activity.RESULT_OK;
+
 public class AddPhotoFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     Spinner spin;
     ImageView cameraButton;
@@ -44,38 +41,42 @@ public class AddPhotoFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public AddPhotoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddPhotoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddPhotoFragment newInstance(String param1, String param2) {
-        AddPhotoFragment fragment = new AddPhotoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_add_photo, container, false);
+
+        galleryLayout = (LinearLayout) view.findViewById(R.id.galleryLayout);
+        spin = (Spinner) view.findViewById(R.id.blSpinner);
+
+        Database db = new Database(getContext());
+
+        ArrayList<Bucketlist> listOfNames = db.getAllBucketlist();
+
+        db.closeDB();
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item, listOfNames);
+        spin.setAdapter(adapter);
+
+        cameraButton = (ImageView) view.findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File picture = null;
+                try{
+                    picture = createImage();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+                Intent i = new Intent();
+                i.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(picture));
+                if(i.resolveActivity(getActivity().getPackageManager()) != null){
+                    startActivityForResult(i, CAMERA_INTENT);
+                }
+            }
+        });
 
         Button returnButton = (Button) view.findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener(){
@@ -90,40 +91,51 @@ public class AddPhotoFragment extends Fragment {
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_INTENT && resultCode == RESULT_OK){
+            Bitmap image = BitmapFactory.decodeFile(imageLocation);
+            ImageView imageView = new ImageView(getContext());
+            imageView.setImageBitmap(image);
+            galleryLayout.addView(imageView);
+            /**
+             * Add the photo to the database
+             */
+            Database db = new Database(getContext());
+            int picID = db.addImage(new Image(imageLocation));
+            if(picID != -1){
+                Bucketlist location = (Bucketlist) spin.getSelectedItem();
+                db.addImageLocation(picID, location.getId());
+                Toast.makeText(getActivity(), "Photo Added",
+                        Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(getActivity(), "Photo Not Added",
+                        Toast.LENGTH_LONG).show();
+            }
+            db.closeDB();
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    File createImage() throws IOException{
+        //Create a timestamp to help create a collision free name
+        String timeStamp =
+                new SimpleDateFormat("yyyyMMdd_HHss").format(new Date());
+        //Create the name of the image
+        String fileName = "hiking_log_" + timeStamp;
+        //Grab the directory we want to save the image
+        File directory =
+                Environment.
+                        getExternalStoragePublicDirectory
+                                (Environment.DIRECTORY_PICTURES);
+        //Create the image in that directory
+        File picture = File.createTempFile(fileName, ".jpg", directory);
+        //Save the location of the image
+        imageLocation = picture.getAbsolutePath();
+        return picture;
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
