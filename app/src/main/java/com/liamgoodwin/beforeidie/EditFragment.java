25f1ec2fee4c303ca7a1e7539589d88b04c75f0e
Liam Goodwin <goodwin_liam1@stclairconnect.ca>
2017-04-02 18:43:40 -0400

    Working on loading names into the spinner

--- app/src/main/java/com/liamgoodwin/beforeidie/EditFragment.java
+++ app/src/main/java/com/liamgoodwin/beforeidie/EditFragment.java
@@ -7,8 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class EditFragment extends Fragment {
+
+    Spinner editSpin;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,6 +22,14 @@ public class EditFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_edit, container, false);
 
+        editSpin = (Spinner) view.findViewById(R.id.blEditSpinner);
+
+        Database db = new Database(getContext());
+        ArrayList<Bucketlist> listOfNames = db.getAllBucketlist();
+        db.closeDB();
+
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item, listOfNames);
+        editSpin.setAdapter(adapter);
 
         // Inflate the layout for this fragment
         return view;
