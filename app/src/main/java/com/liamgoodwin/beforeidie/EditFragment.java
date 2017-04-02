87e59e25232d984ccea85015e5d8cef58ff11301
Liam Goodwin <goodwin_liam1@stclairconnect.ca>
2017-04-02 18:53:38 -0400

    Submit button code is implemented

--- app/src/main/java/com/liamgoodwin/beforeidie/EditFragment.java
+++ app/src/main/java/com/liamgoodwin/beforeidie/EditFragment.java
@@ -1,13 +1,17 @@
 package com.liamgoodwin.beforeidie;
 
 import android.content.Context;
+import android.icu.util.Calendar;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -20,6 +24,8 @@ public class EditFragment extends Fragment {
     EditText name;
     EditText description;
     DatePicker date;
+    FragmentManager fm;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,6 +44,31 @@ public class EditFragment extends Fragment {
 
         ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item, listOfNames);
         editSpin.setAdapter(adapter);
+
+        //Get the name, description and date and set them to the fields when an item is selected in the spinner
+
+        Button submit = (Button) view.findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(date.getYear(), date.getMonth(), date.getDayOfMonth());
+                long millis = calendar.getTimeInMillis();
+
+                Bucketlist bucketlist = new Bucketlist(name.getText().toString(),
+                        description.getText().toString(), millis);
+
+                Database db = new Database(getContext());
+                db.updateBucketlist(bucketlist);
+                db.closeDB();
+
+                fm = getActivity().getSupportFragmentManager();
+                fm.popBackStack();
+
+            }
+        });
 
 
 
