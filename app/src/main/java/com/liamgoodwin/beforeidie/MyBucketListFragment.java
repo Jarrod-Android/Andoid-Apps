ab9353322b53f38116c3673ef7adf28cba75f60b
Liam Goodwin <goodwin_liam1@stclairconnect.ca>
2017-04-02 19:36:53 -0400

    Fixing merging conflicts

--- app/src/main/java/com/liamgoodwin/beforeidie/MyBucketListFragment.java
+++ app/src/main/java/com/liamgoodwin/beforeidie/MyBucketListFragment.java
@@ -189,32 +189,30 @@ public class MyBucketListFragment extends Fragment {
             tweetIntent.putExtra(Intent.EXTRA_TEXT, "This is a Test.");
             tweetIntent.setType("text/plain");
 
-            PackageManager packManager = getPackageManager();
-            List<ResolveInfo> resolvedInfoList = packManager.queryIntentActivities(tweetIntent,  PackageManager.MATCH_DEFAULT_ONLY);
+            //PackageManager packManager = getPackageManager();
+           // List<ResolveInfo> resolvedInfoList = packManager.queryIntentActivities(tweetIntent,  PackageManager.MATCH_DEFAULT_ONLY);
 
-            boolean resolved = false;
-            for(ResolveInfo resolveInfo: resolvedInfoList){
-                if(resolveInfo.activityInfo.packageName.startsWith("com.twitter.android")){
-                    tweetIntent.setClassName(
-                            resolveInfo.activityInfo.packageName,
-                            resolveInfo.activityInfo.name );
-                    resolved = true;
-                    break;
-                }
-            }
-            if(resolved){
-                startActivity(tweetIntent);
-            }else{
-                Intent i = new Intent();
-                i.putExtra(Intent.EXTRA_TEXT, "Hello Download our app");
-                i.setAction(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("https://twitter.com/intent/tweet?text=" + (message));
-                startActivity(i);
-               // Toast.makeText(this, "Twitter app isn't found", Toast.LENGTH_LONG).show();
-            }
-
-
-
+//            boolean resolved = false;
+//            for(ResolveInfo resolveInfo: resolvedInfoList){
+//                if(resolveInfo.activityInfo.packageName.startsWith("com.twitter.android")){
+//                    tweetIntent.setClassName(
+//                            resolveInfo.activityInfo.packageName,
+//                            resolveInfo.activityInfo.name );
+//                    resolved = true;
+//                    break;
+//                }
+//            }
+//            if(resolved){
+//                startActivity(tweetIntent);
+//            }else{
+//                Intent i = new Intent();
+//                i.putExtra(Intent.EXTRA_TEXT, "Hello Download our app");
+//                i.setAction(Intent.ACTION_VIEW);
+//                i.setData(Uri.parse("https://twitter.com/intent/tweet?text=" + (message));
+//                startActivity(i);
+//               // Toast.makeText(this, "Twitter app isn't found", Toast.LENGTH_LONG).show();
+//            }
+//
             final String emailItem = item.getName();
 
             email = (ImageView) convertView.findViewById(R.id.email);
