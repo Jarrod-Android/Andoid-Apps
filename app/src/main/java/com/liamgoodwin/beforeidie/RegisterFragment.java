d7c9735869d6b873dddef592d9bfcb7ad46f3d69
Liam Goodwin <goodwin_liam1@stclairconnect.ca>
2017-04-13 04:03:36 -0400

    Added the views for the RegisterFragment

--- app/src/main/java/com/liamgoodwin/beforeidie/RegisterFragment.java
+++ app/src/main/java/com/liamgoodwin/beforeidie/RegisterFragment.java
@@ -2,14 +2,38 @@ package com.liamgoodwin.beforeidie;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterFragment extends Fragment {
 
+    TextView errorMessage;
+    EditText username;
+    EditText password;
+    EditText password2;
+    CheckBox privacy;
+    Button addAccount;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
 
+        errorMessage = (TextView) view.findViewById(R.id.errorMessage);
+        username = (EditText) view.findViewById(R.id.username);
+        password = (EditText) view.findViewById(R.id.password);
+        password2 = (EditText) view.findViewById(R.id.password2);
+        privacy = (CheckBox) view.findViewById(R.id.privacy);
+        password = (EditText) view.findViewById(R.id.password);
+        addAccount = (Button) view.findViewById(R.id.addAccount);
+
+        return view;
     }
 
 }
